8f3b633eb291e7614c8194af06e1c0c9e2f901bd
RH <52927295@qq.com>
2019-12-18 18:00:55 +0800

    开关控制

--- app/src/main/java/com/example/jetpack/lifecycles/MyLocationListener.java
+++ app/src/main/java/com/example/jetpack/lifecycles/MyLocationListener.java
@@ -1,25 +1,20 @@
 package com.example.jetpack.lifecycles;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.GpsStatus;
-import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
-import android.os.Build;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
-import androidx.lifecycle.LifecycleRegistry;
 import androidx.lifecycle.OnLifecycleEvent;
-
 
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
@@ -37,21 +32,23 @@ public class MyLocationListener implements LifecycleObserver {
     private LocationListener callback;
     private boolean enabled = false;
     private Lifecycle lifecycle;
-     RxPermissions rxPermissions ;
+    RxPermissions rxPermissions;
+
     public MyLocationListener(FragmentActivity context, Lifecycle lifecycle, LocationListener callback) {
         this.lifecycle = lifecycle;
         this.context = context;
-        this.callback =callback;
-        rxPermissions=new RxPermissions(context);
+        this.callback = callback;
+        rxPermissions = new RxPermissions(context);
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     void start() {
         if (!enabled) {
             // connect
-            gpsStart("GPS",5,50,callback);
+            gpsStart("GPS", 5, 50, callback);
         }
     }
+
     public void enable() {
         enabled = true;
         if (lifecycle.getCurrentState().isAtLeast(STARTED)) {
@@ -66,20 +63,9 @@ public class MyLocationListener implements LifecycleObserver {
         gpsStop(callback);
     }
 
+    @SuppressLint("CheckResult")
     private synchronized void gpsStart(String provider, long minTime, float minDistance,
-                                      LocationListener mLocationListener) {
-        if (ContextCompat.checkSelfPermission(context,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-            }else{
-                ActivityCompat.requestPermissions((Activity) context,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        0);
-
-            }
-            return;
-        }
+                                       LocationListener mLocationListener) {
         rxPermissions
                 .request(Manifest.permission.ACCESS_FINE_LOCATION)
                 .subscribe(granted -> {
@@ -88,6 +74,7 @@ public class MyLocationListener implements LifecycleObserver {
                         mLocationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
                         mProvider = mLocationManager.getProvider(LocationManager.GPS_PROVIDER);
                         if (!enabled) {
+                            enabled =true;
                             mLocationManager.requestLocationUpdates(mProvider.getName(), minTime, minDistance, mLocationListener);
                         }
                     } else {
@@ -96,9 +83,10 @@ public class MyLocationListener implements LifecycleObserver {
                 });
 
     }
+
     private synchronized void gpsStop(LocationListener listener) {
         if (enabled) {
-            enabled = !enabled;
+            enabled =false;
             if (mLocationManager != null) {
                 mLocationManager.removeUpdates(listener);
             }
